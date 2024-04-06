b54d3a17c72cf979a265d9f1db31d639a7e9dfd7
ssmf <chamberkid1@gmail.com>
2024-04-06 15:28:56 +0200

    Initialized project + navbar

---
+++ components/navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="Navbar row">
+        <a target="_blank" href="goat">Home</a>
+        <a target="_blank" href="goat">What's new</a>
+        <a target="_blank" href="goat">About</a>
+        <a target="_blank" href="goat">Newsletter</a>
+    </div>
+</template>
+
+<style scoped>
+.Navbar {
+    background-color: black;
+    height: 80px;
+    gap: 50px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <Navbar></Navbar>
+</template>
+
+<style scoped>
+
+</style>
